9ef71063f4f65c3eaa463002b1160645dad6af66
Miguel <Miguel@miguel-PC>
2016-04-25 10:26:50 +0200

    commit inicial

---
+++ src/cdtwitter/CDtwitter.java
@@ -0,0 +1,20 @@
+package cdtwitter;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+/**
+ *
+ * @author Miguel
+ */
+public class CDtwitter {
+    public static void main(String[] args) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter mitwitter = tf.getInstance();
+        
+        Status miStatus = mitwitter.updateStatus("");
+        System.out.println(miStatus.getText());
+    }
+}
